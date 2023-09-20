1f3aaadbd76bbda9d63998bdd121db2881793ad6
syfxlin <i@ixk.me>
2023-09-20 23:13:27 +0800

    style: update artalk style

--- src/components/widgets/artalk/styles.css.ts
+++ src/components/widgets/artalk/styles.css.ts
@@ -3,4 +3,14 @@ import { theme } from "../../../theme/theme.css";
 
 export const container = styled.css`
   margin: ${theme.spacing.calc(4)} 0;
+
+  &.artalk {
+    --at-color-main: ${theme.color.primary.text};
+    --at-color-bg: ${theme.color.background.x1};
+    --at-color-font: ${theme.color.text.x2};
+    --at-color-deep: ${theme.color.text.x2};
+    --at-color-sub: ${theme.color.text.x3};
+    --at-color-grey: ${theme.color.text.x3};
+    --at-color-meta: ${theme.color.text.x3};
+  }
 `;
