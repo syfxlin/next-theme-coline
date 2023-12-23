80ca5431eb8feb966a36dbecba475dc17a2b83c9
syfxlin <i@ixk.me>
2023-12-23 23:51:38 +0800

    fix: vercel not working

--- src/contents/reader.ts
+++ src/contents/reader.ts
@@ -2,6 +2,6 @@ import path from "path";
 import config from "../../keystatic.config";
 import { createReader } from "@keystatic/core/reader";
 
-export const root = path.resolve("public");
+export const root = path.resolve("public/content");
 
 export const reader = createReader(".", config);
