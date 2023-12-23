f393f312c2692192ed9b54cf08080add25ffb0be
syfxlin <i@ixk.me>
2023-12-23 22:36:26 +0800

    fix: vercel not working

--- src/contents/reader.ts
+++ src/contents/reader.ts
@@ -1,5 +1,7 @@
-import fs from "fs-extra";
+import path from "path";
 import config from "../../keystatic.config";
 import { createReader } from "@keystatic/core/reader";
 
-export const reader = createReader(fs.pathExistsSync("public") ? "." : "../", config);
+export const root = path.resolve("public");
+
+export const reader = createReader(".", config);
